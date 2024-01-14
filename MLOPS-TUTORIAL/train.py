79db75a9b1849c2537d2508469dac1c7c806518f
eunbin777 <eunbinkim777@gmail.com>
2024-01-14 22:52:05 +0900

    HPO with Optuna & MLflow

--- MLOPS-TUTORIAL/train.py
+++ MLOPS-TUTORIAL/train.py
@@ -1,43 +1,62 @@
+import uuid
+import optuna
 import mlflow
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-#
-# set mlflow
-#
-mlflow.set_tracking_uri("http://localhost:5001")
-mlflow.set_experiment("tutorial")
+UNIQUE_PREFIX = str(uuid.uuid4())[:8]
 
-with mlflow.start_run():
-    #
-    # log params
-    #
-    params = {"n_estimators": 100, "max_depth": 5}
-    mlflow.log_params(params)
+def objective(trial):
+    # suggest new pearameters
+    trial.suggest_int("n_estimators", 100, 1000, step=100)
+    trial.suggest_int("max_depth", 3, 10)
 
-    #
-    # load data
-    #
-    iris = load_iris(as_frame=True)
-    X, y = iris["data"], iris["target"]
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.3, random_state=2024)
+    run_name = f"{UNIQUE_PREFIX}-{trial.number}"
+    with mlflow.start_run(run_name=run_name):
+        #
+        # log params
+        #
+        mlflow.log_params(trial.params)
 
-    #
-    # train model
-    #
-    clf = RandomForestClassifier(n_estimators=params["n_estimators"], max_depth=params["max_depth"], random_state=2024)
-    clf.fit(X_train, y_train)
+        #
+        # load data
+        #
+        iris = load_iris(as_frame=True)
+        X, y = iris["data"], iris["target"]
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.3, random_state=2024)
 
-    #
-    # evaluate train model
-    #
-    y_pred = clf.predict(X_valid)
-    acc_score = accuracy_score(y_valid, y_pred)
+        #
+        # train model
+        #
+        clf = RandomForestClassifier(n_estimators=trial.params["n_estimators"], max_depth=trial.params["max_depth"], random_state=2024)
+        clf.fit(X_train, y_train)
 
+        #
+        # evaluate train model
+        #
+        y_pred = clf.predict(X_valid)
+        acc_score = accuracy_score(y_valid, y_pred)
+
+        #
+        # log metrics
+        #
+        print("Accuracy: {:.4f}".format(acc_score))
+        mlflow.log_metric("accuracy", acc_score)
+    return acc_score
+
+if __name__ == "__main__":
     #
-    # log metrics
+    # set mlflow
     #
-    print("Accuracy: {:.4f}".format(acc_score))
-    mlflow.log_metric("accuracy", acc_score)
+    study_name = "hpo-tutorial"
+    mlflow.set_tracking_uri("http://localhost:5001")
+    mlflow.set_experiment("hpo-tutorial")
+
+    # study
+    sampler = optuna.samplers.RandomSampler(seed=2024)
+    study = optuna.create_study(sampler=sampler, study_name=study_name, direction="maximize")
+
+    # optimize
+    study.optimize(objective, n_trials=5)
